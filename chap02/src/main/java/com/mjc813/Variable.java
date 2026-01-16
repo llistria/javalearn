9b197fbf52bfb67c3cfed619bada046580d2c507
llistria <jseohyeon57@gmail.com>
2026-01-16 11:21:56 +0900

    chap02 start

---
+++ chap02/src/main/java/com/mjc813/Variable.java
@@ -0,0 +1,22 @@
+package com.mjc813;
+
+public class Variable {
+
+     public void howToUseVariable(){
+         int result1 = 10 + 2 + 8;
+         System.out.println("result1: "+result1);
+
+         String result2 = 10 + 2 + "8";
+         System.out.println("result1: "+result2);
+
+         String result3 = 10 + "2" + "8";
+         System.out.println("result1: "+result3);
+
+         String result4 = "10" + 2 + 8;
+         System.out.println("result1: "+result4);
+
+         String result5 = "10" + (2 + 8);
+         System.out.println("result1: "+result5);
+
+     }
+}
